330b8cac18082b0226b29b5f68bb66c738488a81
akguni <akguni@yahoo.com>
2021-04-12 10:01:16 +0200

    using js instead of django to populate optionlist

--- carbonator/static/carbonator/index.js
+++ carbonator/static/carbonator/index.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // by using no let or const, costs is declared as a global variable
+    // Not using let or const, so that these variables are recognized outside the function
     costs = JSON.parse(document.getElementById('costs').textContent);
+    appliances = JSON.parse(document.getElementById('appliances').textContent);
+    populateAppliances();
     events();
     recalculate();
 })
@@ -15,13 +17,24 @@ function recalculate() {
     const appliance = document.getElementById("appliance");
     const duration = document.getElementById("duration");
     const watts = parseFloat(appliance.options[appliance.selectedIndex].value);
-    const time = parseInt(duration.options[duration.selectedIndex].text);
-    const kWh = (watts * time) / 1000;
-    
+    const timemin = parseInt(duration.options[duration.selectedIndex].text);
+    const timehr  = timemin / 60;
+    const kWh = (watts * timehr) / 1000;    
     const cents = document.getElementById("cents");
-    cents.innerHTML = kWh * costs.cents;
+    cents.innerHTML = (kWh * costs.cents).toFixed(2);
     const co2e = document.getElementById("co2e");
-    co2e.innerHTML = kWh * costs.co2e;
+    co2e.innerHTML = (kWh * costs.co2e).toFixed(2);
     const trees = document.getElementById("trees");
-    trees.innerHTML = kWh * costs.trees;
+    trees.innerHTML = (kWh * costs.trees).toFixed(2);
+}
+
+function populateAppliances() {
+    const applianceList = document.getElementById("appliance");
+    applianceList.innerHTML = "";
+    appliances.forEach(appliance => {
+        const option = document.createElement('option');
+        option.value = appliance.watts;
+        option.innerHTML = appliance.name;
+        applianceList.append(option)        
+    })    
 }
