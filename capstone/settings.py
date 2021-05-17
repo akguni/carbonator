532e6b9def10237da43b29519a2e409190c8ae1a
akguni <akguni@yahoo.com>
2021-05-17 12:35:12 +0200

    migrated from mysql to postgres#

--- capstone/settings.py
+++ capstone/settings.py
@@ -71,15 +71,24 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'capstone.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'carbonator',
+
+        'USER': 'carbonator',
+
+        'PASSWORD': 'carbonator',
+
+        'HOST': 'localhost',
+
+        'PORT': '',
+
     }
+
 }
 
 AUTH_USER_MODEL = 'carbonator.User'
