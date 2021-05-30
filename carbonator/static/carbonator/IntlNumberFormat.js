01943ca6afdafd1e95769c03d822e6fc9ad62a88
akguni <akguni@yahoo.com>
2021-05-30 21:12:26 +0200

    Styled login and register pages

--- carbonator/static/carbonator/IntlNumberFormat.js
+++ carbonator/static/carbonator/IntlNumberFormat.js
@@ -1,9 +1,9 @@
 function displayCurrency(number) {
-    return new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(number);
+    return new Intl.NumberFormat('en-GB', { style: 'currency', currency: 'EUR' }).format(number);
 }
 
 function displayNumber(number) {
-    return new Intl.NumberFormat('de-DE', { maximumFractionDigits: 2}).format(number);
+    return new Intl.NumberFormat('en-GB', { maximumFractionDigits: 2}).format(number);
 }
 
 function displayDate(date) {
