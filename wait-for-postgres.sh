d2d8d0ddce1042a0441724f5792d80ea9442147c
akguni <akguni@yahoo.com>
2021-06-08 14:15:23 +0200

    attempting to correct test yml

--- wait-for-postgres.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# wait-for-postgres.sh
-
-until PGPASSWORD="carbonator" psql -h "postgres" -U "carbonator" -c '\q'; do
-  >&2 echo "Postgres is unavailable - sleeping"
-  sleep 1
-done
-  
->&2 echo "Postgres is up - exiting"
